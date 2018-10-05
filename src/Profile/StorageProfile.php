47faee0170a1989db165e91fc70d195e552ae178
Dieter Vanmoortel <dieter.vanmoortel@delta.blue>
2018-10-05 15:14:35 +0200

    Added option to create empty datadisk with custom name

--- src/Profile/StorageProfile.php
+++ src/Profile/StorageProfile.php
@@ -19,12 +19,16 @@ class StorageProfile
      * @param int $size
      *      Disk size in GB
      */
-    public function addEmptyDataDisk($size = 8)
+    public function addEmptyDataDisk($size = 8, $name = null)
     {
-        $this->dataDisks[] = [
+         $disc = [
             "diskSizeGB" => $size,
             "createOption" => "Empty",
             "lun" => count($this->dataDisks) + 1,
         ];
+         if ($name) {
+             $disc["name"] = $name;
+         }
+        $this->dataDisks[] = $disc;
     }
 }
